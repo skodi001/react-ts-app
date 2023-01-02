8d8e5fc03c49e3b3c896a0482a4012ac48d31d3a
Shokhmurod <shokhpjwstk@gmail.com>
2023-01-02 02:39:55 +0100

    added a data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Lear React"), new Todo("Learn Js")];
   return (
     <div>
-      <Todos items={["Learn React"]} />
+      <Todos items={todos} />
     </div>
   );
 }
